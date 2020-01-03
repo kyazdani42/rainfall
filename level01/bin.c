4aa4dd0bdfc107c58d555090e9ce60004a09b366
kyazdani42 <yazdani.kiyan@protonmail.com>
2020-01-03 13:37:44 +0100

    add level01

--- level01/bin.c
+++ level01/bin.c
@@ -1,5 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 int     main(int ac, char **av)
 {
     char buf[64];
     gets(buf);
+}
+
+void    run() {
+    fwrite("Good... Wait what?\n", 13, 1, stdout);
+    system("/bin/sh");
 }
