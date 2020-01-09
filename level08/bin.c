8d8acc9c736468dfb80ab8194db5d06deece8e5b
kyazdani42 <yazdani.kiyan@protonmail.com>
2020-01-09 17:02:29 +0100

    start rewriting level08 binary in c

---
+++ level08/bin.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int	main()
+{
+	/* ecx -> ds:0x8049ab0 <service> */
+	/* edx -> ds:0x8049aac <auth> */
+	printf("%p, %p \n", edx, ecx);
+
+	fgets(addr of esp+0x20, 128, stdin);
+	if (something == something)
+		return 0;
+	/* ... */
+
+	if (esi < edi)
+	{
+		malloc(4);
+	}
+
+}
