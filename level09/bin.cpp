79a6c2cac51538daef310cacf51e1f7843a70a80
kyazdani42 <yazdani.kiyan@protonmail.com>
2020-01-11 17:46:01 +0100

    level09

--- level09/bin.cpp
+++ level09/bin.cpp
@@ -1,17 +1,40 @@
+#include <stdlib.h>
+#include <string.h>
+
 class N {
+	private:
+		int value;
+		char nstr[108];
+
 	public:
-		virtual operator+() {}
-		virtual operator-() {}
-		static int N(void *ptr, int param) {
+		virtual N operator + (N &n) {
+			return (N(value + n.value));
 		}
-}
+
+		virtual N operator - (N &n) {
+			return (N(value - n.value));
+		}
+
+		void	setAnnotation(char *v) {
+			memcpy(nstr, v, strlen(v));
+		}
+
+		N(int v) {
+			value = v;
+		}
+};
 
 int	main(int ac, char **av)
 {
 	if (ac < 2)
 		exit(1);
 
-	int *v = new 108;
-	N::N(v, 5);
+	N *ptr = new N(5);
+	N *ptr2 = new N(6);
 
+	ptr->setAnnotation(av[1]);
+
+	*ptr2 + *ptr;
+
+	return (0);
 }
